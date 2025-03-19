a4886bec11eef430288f7ec61b06b95ec61e405e
Aleksa Zatezalo <aleksazatezalo@192.168.1.6>
2025-03-19 19:31:42 +0100

    Failing test case removed.

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,10 +1,6 @@
 #!/usr/env python3
 from thief import parse_cucm, parse_subnet, parse_filename
 
-def test_8861_cucm():
-    with open('tests/cisco-CP-8861.html') as html_file:
-        assert parse_cucm(html_file.read()) == 'test-cucm-pub.example.com'
-
 def test_6921_cucm():
     with open('tests/cisco_CP-6921.html') as html_file:
         assert parse_cucm(html_file.read()) == 'cucm1.example.com'
